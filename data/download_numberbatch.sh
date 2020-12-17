62b77b4efb2c7a53123f0eb7dfd248e541756b59
Diwank Singh Tomer <diwank.singh@gmail.com>
2020-12-17 13:00:37 +0000

    feat: Project init
    Signed-off-by: Diwank Singh Tomer <diwank.singh@gmail.com>

---
+++ data/download_numberbatch.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+wget https://conceptnet.s3.amazonaws.com/downloads/2019/numberbatch/numberbatch-en-19.08.txt.gz
+gunzip numberbatch-en-19.08.txt.gz
---
+++ tests/test_spacy_numberbatch.py
@@ -0,0 +1,5 @@
+from spacy_numberbatch import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
